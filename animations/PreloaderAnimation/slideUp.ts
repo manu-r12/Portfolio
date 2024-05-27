7fc61291a11b601fcec3910f5eb850edb9cd436a
Manu R <manurajbhar12@gmail.com>
2024-05-27 21:25:55 +0530

    made some changes in preloader and deleted uncessary files ✅

--- animations/PreloaderAnimation/slideUp.ts
+++
@@ -1,9 +0,0 @@
-export const slideUp = {
-    initial:{
-        y: "0"
-    },
-    exit:{
-        y: "100vh",
-        transitions: {duration: 0.8 , ease: [0.76, 0, 0.24, 1], delay: 0.3}
-    }
-}
